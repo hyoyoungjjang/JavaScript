38f29a4546255b515a9999e4f12f6d6706544af2
hyoyoungjjang <khy20030930@gmail.com>
2024-03-22 16:00:58 +0900

    Reapply "JSP"
    This reverts commit f08f235b175fe81352f2c0c8a60b577749988d3f.

---
+++ resources/js/1.hoisting.js
@@ -0,0 +1,18 @@
+/**
+ * Hoisting
+ */
+
+console.log(name); // name
+var name1= "hyo";
+console.log(name1);
+
+/**
+ * Hoisting은 뭘까?
+ * 
+ * 모든 변수 선언문이 코드의 최상단으로 이동되는 것처럼 느껴지는 현상
+ * let, const
+ */
+
+
+console.log(name2);
+let name2 = "hyo";
